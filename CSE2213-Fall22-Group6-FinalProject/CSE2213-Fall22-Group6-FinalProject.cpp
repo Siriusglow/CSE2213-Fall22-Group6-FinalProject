e3d56aa9521a8b254ae9edb3bf7deca33659cb5d
Siriusglow <gjs153@msstate.edu>
2022-11-01 14:21:02 -0500

    Turned the main function from testing to menuing from which to build our process off of.

--- CSE2213-Fall22-Group6-FinalProject/CSE2213-Fall22-Group6-FinalProject.cpp
+++ CSE2213-Fall22-Group6-FinalProject/CSE2213-Fall22-Group6-FinalProject.cpp
@@ -3,26 +3,135 @@
 #include "User.h"
 using namespace std;
 
+bool login();
+
 int main()                                                                  // The entirity of the main function currently is testing. Class implimentation seems to work. 
 {
-    cout << "Testing GIT" << endl;
-    cout << "Testing to see if merge likes me this time" << endl;
-    cout << "Testing 123" << endl;
-    cout << "This is a branch" << endl;
-    cout << "It's still not updating" << endl;
-    cout << "Testing2" << endl;
+    bool validLogIn = false;
+    cout << "Welcome to Penguin Games. The best place for you that we can deliver!" << endl;
+    int MenuOption = -1;
 
-    User Bob(22, "Bob Smith", "Address", "test@gmail.com", "901-111-1121", "1234-5678-9012", "11/ 12", "123", "Username123", "Password123");
-    User Jack;
-    cout << Bob.getName() << endl;     // testing to ensure the class works and will upload to Github. User class works as intended. However, it's destructor is still not done.
-    cout << Bob.getAddress() << endl;
-    cout << Bob.getEmail() << endl;
-    cout << Bob.getPhone() << endl;
-    cout << Bob.getCreditCardNum() << endl;
-    cout << Bob.getExpirationDate() << endl;
-    cout << Bob.getCVV() << endl;
-    cout << Bob.getUsername() << endl;
-    cout << Bob.getPassword() << endl;
-    Jack.setAddress("test address");
-    cout << "Jack's Address: " << Jack.getAddress() << endl;
+    while (MenuOption != 3) {
+        cout << "Please choose a menu option" << endl;
+        cout << "1. Login" << endl;
+        cout << "2. Please add a user" << endl;
+        cout << "3. Quit" << endl;
+        cout << "Menu option: ";
+        cin >> MenuOption;
+        if (!cin.fail()) {
+            if (MenuOption == 1) {
+                validLogIn = login();
+                if (validLogIn) {
+                    cout << "Login sucessful" << endl << endl;
+                    cout << "Welcome back to the system" << endl;      // FIXME, maybe a variable to the user.
+                    while (MenuOption != 5) {
+                        cout << "Please choose a menu option" << endl;
+                        cout << "1. View and change user information" << endl;
+                        cout << "2. View shopping cart" << endl;
+                        cout << "3. Add item to shopping cart" << endl;
+                        cout << "4. Remove item to shopping cart" << endl;
+                        cout << "5. Logout" << endl;
+                        cout << "Menu option: ";
+                        cin >> MenuOption; // Validate input FIXME
+                        if (!cin.fail()) {
+                            if (MenuOption == 1) {
+                                MenuOption = -1;
+                                while (MenuOption != 1) {
+                                    cout << endl << "Please choose a menu option" << endl;
+                                    cout << "1. Go Back" << endl;
+                                    cout << "2. Edit Payment Information" << endl;
+                                    cout << "3. Edit Shipping Informaiton" << endl;
+                                    cout << "4. Edit Contact Informaiton" << endl;
+                                    cout << "5. View Account Details" << endl;
+                                    cout << "6. View Order History" << endl;
+                                    cout << "7. Delete Account" << endl;
+                                    cout << "Menu option: ";
+                                    cin >> MenuOption;
+                                    if (!cin.fail()) {
+                                        switch (MenuOption) {
+                                        case 1:
+                                            cout << "FIXME case 1 was chosen" << endl;
+                                            break;
+                                        case 2:
+                                            cout << "FIXME case 2 was called" << endl;
+                                            break;
+                                        case 3:
+                                            cout << "FIXME case 3 was called" << endl;
+                                            break;
+                                        case 4:
+                                            cout << "FIXME case 4 was called" << endl;
+                                            break;
+                                        case 5:
+                                            cout << "FIXME case 5 was called" << endl;
+                                            break;
+                                        case 6:
+                                            cout << "FIXME case 6 was called" << endl;
+                                            break;
+                                        case 7:
+                                            cout << "FIXME case 7 was called" << endl;
+                                            break;
+                                        default:
+                                            cout << "Your option is not valid. Please try again." << endl;
+                                        }
+                                    }
+                                    else {
+                                        cout << "Your menu option was invalid. Please choose a number corresponding to one of the options above" << endl;
+                                        cin.clear();
+                                        cin.ignore(10000, '\n');
+                                        cout << endl;
+                                    }
+                                }
+                            }
+                            else if (MenuOption == 2) {
+                                cout << "FIXME Option 2 was chosen" << endl;
+                            }
+                            else if (MenuOption == 3) {
+                                cout << "FIXME Option 3 was chosen" << endl;
+                            }
+                            else if (MenuOption == 4) {
+                                cout << "FIXME Option 4 was chosen" << endl;
+                            }
+                            else if (MenuOption == 5) {
+                                cout << "FIXME Option 5 was chosen" << endl;
+                            }
+                            else {
+                                cout << "Your menu option was invalid. Please choose one of the menu options above." << endl;
+
+                            }
+                            cout << endl;
+                        }
+                        else {
+                            cout << "Your menu option was invalid. Please choose a number corresponding to one of the options above" << endl;
+                            cin.clear();
+                            cin.ignore(10000, '\n');
+                            cout << endl;
+                        }
+                    }
+                }
+                else {
+                    cout << "Login Failed. Please try again." << endl;
+                }
+            }
+            else if (MenuOption == 2) {
+                cout << "FIXME, Add a user was chosen" << endl;
+            }
+            else if (MenuOption == 3) {
+                cout << "Thank you for using the system. Have a nice day!" << endl;
+            }
+            else {
+                cout << "Your menu option was invalid. Please choose one of the menu options above." << endl;
+            }
+        }
+        else {
+            cout << "Your input is invalid. Please choose a number corresponding to one of the options above" << endl;
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << endl;
+        }
+        cout << endl;
+    }
+}
+
+bool login(){
+    return true;        // FIXME. This is true currently for testing purposes.
 }
