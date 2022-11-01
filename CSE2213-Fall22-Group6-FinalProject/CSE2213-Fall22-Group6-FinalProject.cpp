c582460a93de7036ae86cd6a3ad2bf1b1b6825d9
Siriusglow <gjs153@msstate.edu>
2022-11-01 01:09:55 -0500

    Getting things to work.

--- CSE2213-Fall22-Group6-FinalProject/CSE2213-Fall22-Group6-FinalProject.cpp
+++ CSE2213-Fall22-Group6-FinalProject/CSE2213-Fall22-Group6-FinalProject.cpp
@@ -4,4 +4,9 @@ using namespace std;
 int main()
 {
     cout << "Testing GIT" << endl;
+    cout << "Testing to see if merge likes me this time" << endl;
+    cout << "Testing 123" << endl;
+    cout << "This is a branch" << endl;
+    cout << "It's still not updating" << endl;
+    cout << "Testing2" << endl;
 }
