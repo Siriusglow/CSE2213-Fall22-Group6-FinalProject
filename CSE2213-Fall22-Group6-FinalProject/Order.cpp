0948186dd1eb06470f050bcc22530326f329dc68
Jae-Elemental <jae.whynot@gmail.com>
2022-11-28 21:53:08 -0600

    Added Order Class
    order class for final storage and record keeping

---
+++ CSE2213-Fall22-Group6-FinalProject/Order.cpp
@@ -0,0 +1,73 @@
+#include "Order.h"
+
+Order::Order() {
+	orderNumber = 0;
+	userID = 0;
+	itemList = vector<InventoryItem>();
+	total = 0.00;
+	itemCount = 0;
+	shippingAddress = "";
+	datePlaced = "";
+	extraNotes = "";
+}
+
+Order::Order(int orderNumber, int userID, vector<InventoryItem> itemList, double total, string shippingAddress, string datePlaced, string extraNotes) {
+	this->orderNumber = orderNumber;
+	this->userID = userID;
+	this->itemList = itemList;
+	this->total = total;
+	this->itemCount = itemCount;
+	this->shippingAddress = shippingAddress;
+	this->datePlaced = datePlaced;
+	this->extraNotes = extraNotes;
+}
+
+Order::~Order() {
+	cout << "FIXME Destructor Called" << endl;
+}
+
+void Order::setUserID(int id) {
+	this->userID = id;
+}
+
+void Order::setShippingAddress(string address) {
+	this->shippingAddress = address;
+}
+
+int Order::getOrderNum() {
+	return orderNumber;
+}
+
+int Order::getUserID() {
+	return userID;
+}
+
+double Order::getTotal() {
+	calculateTotal();
+	return total;
+}
+
+string Order::getShippingAddress() {
+	return shippingAddress;
+}
+
+string Order::getNotes() {
+	return extraNotes;
+}
+
+void Order::calculateTotal() {
+	double temptotal = 0;
+	for (int i = 0; i < this->itemList.size(); i++) {
+		temptotal += this->itemList[i].getCost() * this->itemList[i].getCount();
+	}
+	this->total = temptotal;
+}
+
+void Order::addItem(InventoryItem item) {
+	itemList.push_back(item);
+	itemCount++;
+}
+
+void Order::addNote(string note) {
+	this->extraNotes += " - " + note;
+}
---
+++ CSE2213-Fall22-Group6-FinalProject/Order.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include "InventoryItem.h"
+#include "Inv.h"
+using namespace std;
+
+class Order
+{
+private:
+	int orderNumber;
+	int userID;
+	vector<InventoryItem> itemList;
+	double total;
+	int itemCount;
+	string shippingAddress;
+	string datePlaced;
+	string extraNotes;
+public:
+	Order();
+	Order(int orderNumber, int userID, vector<InventoryItem> itemList, double total, string shippingAddress, string datePlaced, string extraNotes);
+	~Order();
+
+	void setUserID(int id);
+	void setShippingAddress(string address);
+
+	int getOrderNum();
+	int getUserID();
+	double getTotal();
+	string getShippingAddress();
+	string getNotes();
+	
+	void calculateTotal();
+	void addItem(InventoryItem item);
+	void addNote(string note);
+};
+
